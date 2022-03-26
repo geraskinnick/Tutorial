689209f607fdcc1af21f9936fecf85f3de30f27b
geraskinnick <geraskinnick@gmail.com>
2022-03-26 20:27:08 +0200

    add.ansvers

--- index.js
+++ index.js
@@ -28,3 +28,28 @@
 // sumNumbers(5, 10);
 
 ////////////////////////////////////
+
+// let base = prompt('Number число');
+// base = Number(base);
+// console.log(base);
+// console.log(typeof base);
+
+// let stepen = prompt("степень");
+// stepen = Number(stepen);
+// console.log(stepen);
+// console.log(typeof base);
+
+// const reult = base ** stepen;
+// result = Number(reult);
+// console.log(reult);
+// console.log(typeof reult);
+
+// const reultt = Math.sqrt(base * stepen);
+// resultt = Number(reultt);
+// console.log(reultt);
+// console.log(typeof reultt);
+
+// const max = 80;
+// const min = 10;
+// const resulttt = Math.round(Math.random() * (max - min) + min);
+// console.log(resulttt)
